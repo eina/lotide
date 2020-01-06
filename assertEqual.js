972cd3f50f223198d09706b14f3471b1823a03ae
Eina Onting <einers@outlook.com>
2020-01-06 22:21:36 +0000

    create and implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('orange', 'orange');
+assertEqual('blue', 'purple');
+assertEqual(100, 50);
+
