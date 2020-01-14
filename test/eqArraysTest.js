f9caf1f60a995a580825ec9d6e1731b7da6d2ca9
Eina Onting <einers@outlook.com>
2020-01-13 18:26:08 -0800

    write a test for eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,2 +1,12 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
+
+describe("#countOnly", () => {
+  it("object {ab} === {ba} true", () => {
+    assert.strictEqual(eqArrays([1, 2], [1, 2]), true);
+  });
+
+  it('should return false', function () {
+    assert.strictEqual( eqArrays([1, 2, 3], [2, 4]), false )
+  });
+});
